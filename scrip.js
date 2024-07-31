9f09feda10e3982a34f4639aa16e7fc3946cc71b
João Luiz <joaoluizmartins6@gmail.com>
2024-07-31 16:14:28 -0300

    improve validation and fix wrong methods

--- scrip.js
+++ scrip.js
@@ -2,19 +2,29 @@ const process = require("node:process");
 
 const userInput = process.argv[2].toUpperCase();
 
-const handsOptions = [{value: "ROCK", counter: "PAPER"}, {value: "PAPER", counter: "SCISSORS"}, {value: "SCISSORS", counter: "ROCK"}];
+const handsOptions = [
+    {value: "ROCK", counter: "PAPER"},
+    {value: "PAPER", counter: "SCISSORS"},
+    {value: "SCISSORS", counter: "ROCK"}
+];
 
-if(!handsOptions.includes(userInput.value)){
+const userChoice = handsOptions.find((choice) => choice.value = userInput)
+
+if(userChoice === undefined){
     console.log("Input must be eaither ROCK, PAPER or SCISSORS!!!")
     return;
 }
 
-let userChoice = handsOptions.find((choice) => choice.value = userInput)
-const machineChoice = handsOptions[Math.floor(Math.random() + 1)].value;
+const machineChoice = handsOptions[Math.floor(Math.random() * handsOptions.length)];
 
-if(userInput === machineChoice.counter){
+console.log(`\nUser: ${userChoice.value}\nMachine: ${machineChoice.value}\n`);
+
+if(userChoice.value === machineChoice.counter){
     console.log("Player wins");
+    return;
 }
 
-if(machineChoice === userInput.cou)
-
+if(machineChoice.value === userChoice.counter){
+    console.log("Machine wins")
+    return;
+}
