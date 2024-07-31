e2a7c6f90efc5134b3682fd5200566cdeecbe7b1
João Luiz <joaoluizmartins6@gmail.com>
2024-07-31 15:45:20 -0300

    add array of option add initial validation

--- scrip.js
+++ scrip.js
@@ -3,3 +3,7 @@ const process = require("node:process");
 const input = process.argv[2].toUpperCase();
 
 const handsOptions = ["ROCK", "PAPER", "SCISSORS"];
+
+if(!handsOptions.find(input)){
+    console.log("Input must bet eaither ROCK, PAPER or SCISSORS!!!")
+}
