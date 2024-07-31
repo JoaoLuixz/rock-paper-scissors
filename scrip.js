9e83fa047479d34f4c8d40b6901e0e5667c2551c
João Luiz <joaoluizmartins6@gmail.com>
2024-07-31 15:46:10 -0300

    fix typo in log message

--- scrip.js
+++ scrip.js
@@ -5,5 +5,5 @@ const input = process.argv[2].toUpperCase();
 const handsOptions = ["ROCK", "PAPER", "SCISSORS"];
 
 if(!handsOptions.find(input)){
-    console.log("Input must bet eaither ROCK, PAPER or SCISSORS!!!")
+    console.log("Input must be eaither ROCK, PAPER or SCISSORS!!!")
 }
