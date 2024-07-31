b752f579c925ff16efb16c6012986d68f48d0caf
João Luiz <joaoluizmartins6@gmail.com>
2024-07-31 15:58:56 -0300

    add variable to save the user choice add variable to save the machine random choice add basic validation to check the winner

--- scrip.js
+++ scrip.js
@@ -1,11 +1,20 @@
 const process = require("node:process");
 
-const input = process.argv[2].toUpperCase();
+const userInput = process.argv[2].toUpperCase();
 
-const handsOptions = ["ROCK", "PAPER", "SCISSORS"];
+const handsOptions = [{value: "ROCK", counter: "PAPER"}, {value: "PAPER", counter: "SCISSORS"}, {value: "SCISSORS", counter: "ROCK"}];
 
-if(!handsOptions.includes(input)){
+if(!handsOptions.includes(userInput.value)){
     console.log("Input must be eaither ROCK, PAPER or SCISSORS!!!")
-
     return;
 }
+
+let userChoice = handsOptions.find((choice) => choice.value = userInput)
+const machineChoice = handsOptions[Math.floor(Math.random() + 1)].value;
+
+if(userInput === machineChoice.counter){
+    console.log("Player wins");
+}
+
+if(machineChoice === userInput.cou)
+
