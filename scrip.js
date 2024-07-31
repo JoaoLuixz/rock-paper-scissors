b1ff8f0c96c49e35906b5a1fd9db1c0a3029d538
João Luiz <joaoluizmartins6@gmail.com>
2024-07-31 15:48:48 -0300

    fix wrong validation method

--- scrip.js
+++ scrip.js
@@ -4,6 +4,8 @@ const input = process.argv[2].toUpperCase();
 
 const handsOptions = ["ROCK", "PAPER", "SCISSORS"];
 
-if(!handsOptions.find(input)){
+if(!handsOptions.includes(input)){
     console.log("Input must be eaither ROCK, PAPER or SCISSORS!!!")
+
+    return;
 }
